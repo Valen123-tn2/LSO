8b5d17c66604a4aba97240f5623d248e30b480fb
Valen123-tn2 <valentinarosascamara@gmail.com>
2025-06-09 09:45:50 -0300

    a

---
+++ Laboratorio SO/Tp12_9-06/1.py
@@ -0,0 +1,8 @@
+n = []
+ind = int(input("Ingrese cantidad de numeros a ingresar: "))
+
+for i in range(ind):
+    num = int(input("Ingrese el numero: "))
+    n.append(num)
+
+print(n)
---
+++ Laboratorio SO/Tp12_9-06/2.py
@@ -0,0 +1,17 @@
+te = []
+ind = int(input("Ingrese la cantidad de temperaturas a agregar: "))
+mm =[]
+mi = 0
+for i in range(ind):
+    tem = float(input("Ingrese temperatura: "))
+    te.append(tem)
+    tt += te[i]
+
+m = tt/ind
+
+for i in range(te):
+    if te[i] >= m:
+        mi += 1
+        mm.append(te[i])
+
+print("La cantidad de temperaturas mayores a la media fueron",mi,"siendo las temperaturas de",mm)
